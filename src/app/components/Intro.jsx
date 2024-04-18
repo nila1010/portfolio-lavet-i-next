92abe97f2dba814c4766b18894c17fbd0e024cd5
NL <nila0001@stud.kea.dk>
2024-04-18 16:51:59 +0200

    v2

--- src/app/components/Intro.jsx
+++ src/app/components/Intro.jsx
@@ -10,7 +10,7 @@ export default function Intro() {
     let tl = gsap.timeline({
       scrollTrigger: {
         trigger: ".slideintext",
-        start: "top 200",
+        start: "top 160",
         end: "top",
         scrub: true,
       },
@@ -24,7 +24,7 @@ export default function Intro() {
       },
     });
 
-    tl.to(".slideintext", { translateX: 600 });
+    tl.to(".slideintext", { translateX: 580 });
     tl2.to(".infotext", { translateX: -600 });
 
     gsap.to(".text1", {
